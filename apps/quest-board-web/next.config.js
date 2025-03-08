0111a04dbb0005dd5cfa675a8c06322a2518cf63
BunkOtherThomas <109066859+BunkOtherThomas@users.noreply.github.com>
2025-03-08 16:29:19 +0000

    Revert "Does this work now?"
    This reverts commit 7a8b0702d87eacc92425f30344a471e2b8404b1f.

--- apps/quest-board-web/next.config.js
+++ apps/quest-board-web/next.config.js
@@ -2,16 +2,12 @@
 const nextConfig = {
   output: 'standalone',
   reactStrictMode: true,
-  transpilePackages: ['@planning/common-utils', '@quest-board/types'],
+  transpilePackages: ["@repo/ui", "@planning/common-utils"],
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
-  experimental: {
-    externalDir: true,
-    outputFileTracingRoot: process.env.VERCEL ? undefined : process.cwd(),
-  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
